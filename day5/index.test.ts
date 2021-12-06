7299a714ac1ec88ea0cca38592a31d3719e570f4
James dockeray <jdockeray20@gmail.com>
2021-12-06 08:59:55 +0000

    da 5 - part 1

---
+++ day5/index.test.ts
@@ -0,0 +1,104 @@
+import { assertEquals } from "https://deno.land/std@0.100.0/testing/asserts.ts";
+import {
+  drawPath,
+  formatRow,
+  formatRows,
+  getHorizontalLine,
+  getVerticalLine,
+  isValidHorizontal,
+  isValidVertical,
+  Path,
+  plotLine,
+} from "./index.ts";
+
+Deno.test("row format", () => {
+  const result = formatRow("661,85 -> 780,85");
+  assertEquals(result, [[661, 85], [780, 85]]);
+});
+
+Deno.test("rows format", () => {
+  const input = ["661,85 -> 780,85", "484,231 -> 91,231"];
+  const result = formatRows(input);
+
+  const output = [[[661, 85], [780, 85]], [[484, 231], [91, 231]]];
+  assertEquals(result, output);
+});
+
+Deno.test("fails when not valid horizontal", () => {
+  const result = isValidHorizontal([[804, 936], [949, 936]]);
+
+  assertEquals(result, false);
+});
+
+Deno.test("passes when  valid horizontal", () => {
+  const result = isValidHorizontal([[9, 7], [8, 7]]);
+
+  assertEquals(result, true);
+});
+
+Deno.test("fails when not valid vertical", () => {
+  const result = isValidVertical([[804, 936], [949, 936]]);
+
+  assertEquals(result, false);
+});
+
+Deno.test("passes when  valid vertical", () => {
+  const result = isValidVertical([[1, 1], [1, 3]]);
+
+  assertEquals(result, true);
+});
+
+Deno.test("gets horizontal line - desc", () => {
+  const input: Path = [[9, 7], [7, 7]];
+  const result = getHorizontalLine(input);
+
+  const output = [[7, 7], [8, 7], [9, 7]];
+  assertEquals(result, output);
+});
+
+Deno.test("gets horizontal line -asc", () => {
+  const input: Path = [[7, 7], [9, 7]];
+  const result = getHorizontalLine(input);
+
+  const output = [[7, 7], [8, 7], [9, 7]];
+  assertEquals(result, output);
+});
+
+Deno.test("gets vertical line - asc", () => {
+  const input: Path = [[1, 1], [1, 3]];
+  const result = getVerticalLine(input);
+
+  const output = [[1, 1], [1, 2], [1, 3]];
+  assertEquals(result, output);
+});
+
+Deno.test("gets vertical line - des", () => {
+  const input: Path = [[1, 3], [1, 1]];
+  const result = getVerticalLine(input);
+
+  const output = [[1, 1], [1, 2], [1, 3]];
+  assertEquals(result, output);
+});
+
+Deno.test("plots line", () => {
+  const map = plotLine(new Map(), [[1, 1], [1, 2], [1, 3]]);
+
+  assertEquals(map.get("1,1"), 1);
+  assertEquals(map.get("1,2"), 1);
+  assertEquals(map.get("1,3"), 1);
+});
+
+Deno.test("counts crossing lines", () => {
+  let map = plotLine(new Map(), [[1, 1], [1, 2], [1, 3]]);
+  map = plotLine(map, [[1, 1], [1, 2], [1, 3]]);
+  assertEquals(map.get("1,1"), 2);
+  assertEquals(map.get("1,2"), 2);
+  assertEquals(map.get("1,3"), 2);
+});
+
+Deno.test("draws vertical path", () => {
+  const map = drawPath(new Map(), [[1, 3], [1, 1]]);
+  assertEquals(map.get("1,1"), 1);
+  assertEquals(map.get("1,2"), 1);
+  assertEquals(map.get("1,3"), 1);
+});
---
+++ day5/index.ts
@@ -0,0 +1,86 @@
+type Cood = [number, number];
+export type Path = [Cood, Cood];
+type PathMap = Map<string, number>;
+
+function numberRange(start: number, end: number): number[] {
+  return new Array(end - start).fill(0).map((d, i) => i + start);
+}
+
+export async function parseData(path = "input.txt") {
+  const text = await (await Deno.readTextFile(path)).split("\n");
+  const paths = formatRows(text);
+
+  let pathMap = new Map();
+  paths.forEach((p) => {
+    pathMap = drawPath(pathMap, p);
+  });
+
+  let count = 0;
+  for (const [_key, value] of pathMap) {
+    if (value >= 2) {
+      count = count + 1;
+    }
+  }
+  return count
+}
+
+export function formatRow(str: string): Path {
+  const [x1, y1, x2, y2] = str.replace(" -> ", ",").split(",").map(parseFloat);
+  return [[x1, y1], [x2, y2]];
+}
+
+export function formatRows(strArr: string[]): Path[] {
+  return strArr.map(formatRow);
+}
+
+export function drawPath(pathMap: PathMap, path: Path): PathMap {
+  if (isValidHorizontal(path)) {
+    return plotLine(pathMap, getHorizontalLine(path));
+  }
+  if (isValidVertical(path)) {
+    return plotLine(pathMap, getVerticalLine(path));
+  }
+  return pathMap;
+}
+
+export function isValidHorizontal([[_x1, y1], [_x2, y2]]: Path): boolean {
+  return y1 === y2;
+}
+
+export function isValidVertical([[x1, _y1], [x2, _y2]]: Path): boolean {
+  return x1 === x2;
+}
+
+export function getVerticalLine([[_x1, y1], [x, y2]]: Path): Cood[] {
+  const min = y1 > y2 ? y2 : y1;
+  const max = y1 > y2 ? y1 : y2;
+  return numberRange(min, max + 1).map((n) => [x, n]);
+}
+
+export function getHorizontalLine([[x1, y], [x2, _y]]: Path): Cood[] {
+  const min = x1 > x2 ? x2 : x1;
+  const max = x1 > x2 ? x1 : x2;
+  return numberRange(min, max + 1).map((n) => [n, y]);
+}
+
+export function plotLine(map: PathMap, line: Cood[]): PathMap {
+  line.forEach((cood) => {
+    const hash = getCoodHash(cood);
+    if (map.has(hash)) {
+      const currentVal = map.get(hash);
+      if (currentVal !== undefined) { // ts needs this for maps
+        map.set(hash, currentVal + 1);
+      }
+    } else {
+      map.set(hash, 1);
+    }
+  });
+  return map;
+}
+
+export function getCoodHash(cood: Cood) {
+  return cood.toString();
+}
+
+console.log(await parseData());
+
