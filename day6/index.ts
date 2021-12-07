b51e09245ce31af145a17cc4c1f5f9203801ee89
James dockeray <jdockeray20@gmail.com>
2021-12-07 09:18:17 +0000

    day 6 - part 2

--- day6/index.ts
+++ day6/index.ts
@@ -1,29 +1,64 @@
 export async function parseData(path = "input.txt") {
-    const numbers = await (await Deno.readTextFile(path)).split(",").map(parseFloat);
-   const fish = changeDays(256, numbers)
-   return fish.length
+  const fish = await (await Deno.readTextFile(path)).split(",").map(
+    parseFloat,
+  );
+  return countManyFish(fish, 256);
 }
 
+// first attempt, brute force by creating and counting arrays
 
-
-export function changeDay(fish: number[]):number[]{
-    const newFish: number[] = []
-    const transformFish = fish.map((f)=>{
-        if(f === 0){
-            newFish.push(8)
-            return 6
-        }
-        return f - 1
-    })
-    return [...transformFish, ...newFish]
+export function changeDay(fish: number[]): number[] {
+  const newFish: number[] = [];
+  const transformFish = fish.map((f) => {
+    if (f === 0) {
+      newFish.push(8);
+      return 6;
+    }
+    return f - 1;
+  });
+  return [...transformFish, ...newFish];
 }
 
 export function changeDays(days: number, fish: number[]): number[] {
-    let transformFish = fish
-    for(let d = 0; d < days; d++){
-        transformFish = changeDay(transformFish)
-    }
-    return transformFish
+  let transformFish = fish;
+  for (let d = 0; d < days; d++) {
+    transformFish = changeDay(transformFish);
+  }
+  return transformFish;
 }
 
-console.log(await parseData())
+// second attempt, memoize and recurse
+
+let memoized: Map<number, Map<number, number>> = new Map();
+
+export const countFish = (fishTime: number, daysLeft: number): number => {
+  if (memoized.get(daysLeft)?.get(fishTime)) {
+    return memoized.get(daysLeft)!.get(fishTime)!;
+  }
+
+  if (daysLeft === 0) {
+    return 1;
+  }
+
+  if (fishTime === 0) {
+    const result = countFish(6, daysLeft - 1) + countFish(8, daysLeft - 1);
+    if (!memoized.has(daysLeft)) {
+      memoized.set(daysLeft, new Map());
+    }
+    memoized.get(daysLeft)?.set(fishTime, result);
+
+    return result;
+  }
+
+  return countFish(fishTime - 1, daysLeft - 1);
+};
+
+export const countManyFish = (fish: number[], daysLeft: number) => {
+  let sum = 0;
+  for (const f of fish) {
+    sum = sum + countFish(f, daysLeft);
+  }
+  return sum;
+};
+
+console.log(await parseData());
--- day6/input.test.ts
+++ day6/input.test.ts
@@ -1,21 +1,38 @@
-import { changeDay, changeDays } from "./index.ts";
+import { changeDay, changeDays, countFish } from "./index.ts";
 import { assertEquals } from "https://deno.land/std@0.100.0/testing/asserts.ts";
 
 Deno.test("changes day correctly -> no new lantern fish", () => {
-    const result = changeDay([3,4,3,1,2]);
+  const result = changeDay([3, 4, 3, 1, 2]);
 
-    assertEquals(result, [2,3,2,0,1]);
+  assertEquals(result, [2, 3, 2, 0, 1]);
 });
 
 Deno.test("changes day correctly -> new lantern fish", () => {
-    const result = changeDay([2,3,2,0,1]);
+  const result = changeDay([2, 3, 2, 0, 1]);
 
-    assertEquals(result, [1,2,1,6,0,8]);
+  assertEquals(result, [1, 2, 1, 6, 0, 8]);
 });
 
+Deno.test("changes days", () => {
+  const result = changeDays(4, [3, 4, 3, 1, 2]);
+
+  assertEquals(result, [6, 0, 6, 4, 5, 6, 7, 8, 8]);
+});
 
 Deno.test("changes days", () => {
-    const result = changeDays(4, [3,4,3,1,2]);
+  const result = changeDays(4, [3, 4, 3, 1, 2]);
 
-    assertEquals(result, [6,0,6,4,5,6,7,8,8]);
+  assertEquals(result, [6, 0, 6, 4, 5, 6, 7, 8, 8]);
+});
+
+Deno.test("changes days", () => {
+  const result = changeDays(20, [6]);
+
+  assertEquals(result, [0, 2, 2, 4]);
+});
+
+Deno.test("count fish", () => {
+  const result = countFish(6, 20);
+
+  assertEquals(result, 4);
 });
